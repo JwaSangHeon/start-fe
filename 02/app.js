27ed0bee4a95b57fece4c6f9d98cacae692dcee9
JwaSangHeon <whk4051@naver.com>
2022-03-10 17:54:53 +0900

    add array CRUD

--- 02/app.js
+++ 02/app.js
@@ -1,44 +1,71 @@
-// 2주차 실습
+// // 2주차 실습
 
-var name = 'sangheon';
-// 이름을 찍는다.
-console.log(name);
+// var name = 'sangheon';
+// // 이름을 찍는다.
+// console.log(name);
 
-var isMan = true;
-var isManString = 'true';
-console.log(isMan);
-console.log(isManString);
+// var isMan = true;
+// var isManString = 'true';
+// console.log(isMan);
+// console.log(isManString);
 
-function log(str){
-  console.log(str);
-}
-log('hello');
+// function log(str){
+//   console.log(str);
+// }
+// log('hello');
 
-var img ={};
-img.width = 100;
-img.height = 200;
-console.log(img);
+// var img ={};
+// img.width = 100;
+// img.height = 200;
+// console.log(img);
 
-var arr = [1,2,3,4,5];
-console.log(arr);
+// var arr = [1,2,3,4,5];
+// console.log(arr);
 
-console.log(1+1);
-console.log(name+'hello');
+// console.log(1+1);
+// console.log(name+'hello');
 
-var me = {
-  age : 25,
-  name: 'sangheon',
-  isMan: true,
-  sayHello: function(){
-    console.log('hello');
-  },
-};
+// var me = {
+//   age : 25,
+//   name: 'sangheon',
+//   isMan: true,
+//   sayHello: function(){
+//     console.log('hello');
+//   },
+// };
 
-me.sayHello();
+// me.sayHello();
 
-console.log(arr.length);
+// console.log(arr.length);
 
-for(var i=0; i<arr.length; i++){
-  console.log(arr[i]);
-}
-console.log(i);
+// for(let i=0; i<arr.length; i++){
+//   console.log(arr[i]);
+// }
+
+// create
+var todos = ['운동'];
+
+var todo = '게임';
+todos.push(todo);
+console.log(todos);
+
+//read
+todos.forEach((todo) => {
+  console.log(todo);
+});
+
+// update
+var updateTodo = '게임';
+var updateIndex = todos.findIndex((todo) =>{
+  return todo === updateTodo;
+});
+console.log(updateIndex);
+todos[updateIndex] = '공부';
+console.log(todos);
+
+
+var deleteTodo = '공부';
+var newTodos = todos.filter(function(todo) {
+  return todo !== deleteTodo;
+});
+console.log(newTodos);
