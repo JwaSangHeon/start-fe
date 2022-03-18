b09dc997b2b4d03823424b83e1e626377eb6531e
JwaSangHeon <whk4051@naver.com>
2022-03-18 16:05:28 +0900

    add 03 quiz

--- 03/arrayCRUD.js
+++ 03/arrayCRUD.js
@@ -1,28 +1,47 @@
 
-function create(){
-  var newArray = ['운동', '게임'];
-  return newArray;
-}
+// function create(){
+//   var newArray = ['운동', '게임'];
+//   return newArray;
+// }
 
-var array = create();
+// var array = create();
 
-function read(array){
-  array.forEach(element => {
-    console.log(element);
-  });
-}
+// function read(array){
+//   array.forEach(element => {
+//     console.log(element);
+//   });
+// }
 
-function update(newElement){
-  array = array.map((element) => {
-    if (element === newElement) {
-      return '공부';
-    }
-    return element;
-  });
-}
+// function update(newElement){
+//   array = array.map((element) => {
+//     if (element === newElement) {
+//       return '공부';
+//     }
+//     return element;
+//   });
+// }
 
-function deletefun(deleteElement){
-  array = array.filter(function(element) {
-    return element !== deleteElement;
-  });
+// function deletefun(deleteElement){
+//   array = array.filter(function(element) {
+//     return element !== deleteElement;
+//   });
+// }
+
+// var my = {
+//   name: 'JwaSangHeon',
+//   age: 25,
+//   sayHello: function(){
+//     console.log('Hello');
+//   }
+// }
+
+var sum = 0;
+while (true) {
+  var num = prompt('수를 입력하세요');
+  sum += Number(num);
+  if (num === null){
+    break;
+  }
 }
+console.log(sum);
+
