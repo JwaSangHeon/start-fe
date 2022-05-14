9052471056042b18718897b8a8d21db6310d40a6
JwaSangHeon <whk4051@naver.com>
2022-05-14 15:05:08 +0900

    검색 구현

--- 11/app.js
+++ 11/app.js
@@ -1,22 +1,34 @@
-const url = "https://dapi.kakao.com/v2/search/web?query=이효리";
-const $docs = document.querySelector(".docs");
-
-function getFetch(url, callback) {
-  const headers = {
-    Authoriztion: "KakaoAK 8e6158141cb85535608c40f1fbab53e0",
-  };
-  fetch(url, { headers })
-    .then((response) => response.json())
-    .then((data) => callback(data));
-}
-
-getFetch(url, (data) => {
+const $btn = document.querySelector("#search-btn");
+const $input = document.querySelector("#search-text");
+const $result = document.querySelector("#result");
+const url = "https://dapi.kakao.com/v2/search/web";
+const headers = {
+  Authorization: "KakaoAK 8e6158141cb85535608c40f1fbab53e0",
+};
+const success = (data) => {
   const { documents } = data;
   console.log(documents);
 
-  const docs = documents.map((document) => {
-    console.log(document);
-    return document.contents;
+  $result.innerHTML = `<ul class="list-group list-group-flush">`;
+  documents.forEach((ele) => {
+    $result.innerHTML += `<li class="list-group-item"><h2><a href=${ele.url}><${ele.title}</a></h2><p>${ele.contents}</p></li>`;
   });
-  $docs.innerHTML = docs;
-});
+  $result.innerHTML += `</ul>`;
+};
+const error = () => {
+  alert("데이터를 가져올 수 없습니다");
+};
+
+const search = () => {
+  const { value } = $input;
+  if (value === "") {
+    alert("검색어를 입력하세요");
+    return;
+  }
+  fetch(`${url}?query=${value}`, { headers })
+    .then((res) => res.json())
+    .then(success)
+    .catch(error);
+};
+
+$btn.addEventListener("click", search);
